c34174753ae50c20e2c000aff2c4ed16a74aaf81
Ethan Peacock <peacockethan@gmail.com>
2021-02-24 08:22:40 -0500

    Clean up code and remove redundancies

---
+++ Examples/OrbitControlsExample/script.js
@@ -0,0 +1,52 @@
+import * as THREE from '/jsm/three.module.js';
+import { OrbitControls } from '/jsm/OrbitControls.js';
+
+var camera, controls, scene, renderer;
+
+init();
+
+animate();
+
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xcccccc)
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+    renderer = new THREE.WebGLRenderer( {antialias: true} );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(10, 10, 10);
+    camera.lookAt(0, 0, 0);
+
+    // TODO: remove axes helper
+    var axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
+
+    // TODO: implement controls
+    controls = new OrbitControls( camera, renderer.domElement);
+
+    // TODO: add a box to visualize rotation
+    // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    // var material = new THREE.MeshBasicMaterial( {color: 0x00ff00}, wireframe=true );
+    var geometry = new THREE.TorusKnotBufferGeometry( 3, 1, 256, 32 );
+    var material = new THREE.MeshStandardMaterial( { color: 0x6083c2 } );
+
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+
+
+
+}
+
+function animate() {
+    requestAnimationFrame( animate);
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
